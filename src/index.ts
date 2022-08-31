30983b006cf96a4a84d4126864d2b15fbcba785b
Lincoln <lincoln.reboucas40@gmail.com>
2022-08-30 21:40:11 -0300

    Aula 12.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import validator from 'validator';
-
-let name: string = "lcu";
-
-if(validator.isLowercase(name)){
-    console.log(`A string ${name} é toda minuscula`);
-}else {
-    console.log(`A string ${name} não é toda minuscula`);
-}
