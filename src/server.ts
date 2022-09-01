a57e99a7b3d01bce120a96591576806baa76ba7a
Lincoln <lincoln.reboucas40@gmail.com>
2022-08-31 21:55:43 -0300

    Aula 17 - Pasta publica e arquivos estaticos

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 import mainRouters from './routers/index';
 import painelRouters from './routers/painel';
 
 const server = express();
+
+server.use(express.static(path.join(__dirname, '../public')));
 
 server.use(mainRouters);
 server.use('/painel', painelRouters);
