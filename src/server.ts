cb2da774665a80b76721f2dfdedd8555b6d3b8b4
Lincoln <lincoln.reboucas40@gmail.com>
2022-08-31 21:08:49 -0300

    Aula 14

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,26 @@ import express, { Request, Response} from 'express';
 
 const server = express();
 
-server.get('/', (req: Request, res: Response)=>{
-    res.send('Olá Mundo!');
+const Homepage = (req: Request, res: Response)=>{
+    let slug: string = req.params.slug;
+    res.send(`Notícia: ${slug}`);
+}; 
+
+
+server.get('/noticia/:slug', Homepage);
+
+server.get('/noticia/:slug', (req: Request, res: Response)=>{
+    let slug: string = req.params.slug;
+    res.send(`Notícia: ${slug}`);
 });
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response)=>{
+    let { origem, destino } = req.params;
+
+    res.send(`Procurando voos de ${origem.toUpperCase()} até ${destino.toUpperCase()}`);
+});
+
+//site.com/voo/gru-rec
+
 
 server.listen(80);
